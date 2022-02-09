54673cecad2b0f4b50e97d22d3b6c82d224c7da6
Samyuktha_Santhosh <samyukthasanthosh2203@gmail.com>
2022-02-09 19:56:21 +0530

    mini book

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from'./app';
-import './index.css';
+import './book.css';
 
-
-const Main=() =>{
-    return(
-      <>
-      <App/>
-      </>
-    );
+function BookList(){
+return(
+  <section className='booklist'>
+    <Book/>
+  </section>
+)
+}
+const author='Sudha Murty'
+const Book=() =>{
+  const title='Grandmas Bag of Stories'
+  return (
+  <article className='book'>
+    <img src="https://images-eu.ssl-images-amazon.com/images/I/61ck5XWxB3L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg" alt=""/>
+    <h1>{title}</h1>
+    <h4>{author.toUpperCase()}</h4>
+    <p>{6+6}</p>
+  </article>
+  )
 }
 
-ReactDom.render(<Main/>,document.getElementById('root'))
+
+
+ReactDom.render(<BookList/>,document.getElementById('root'))
