164fa21f570a08dac1069b9814495ceb0e93751b
Samyuktha_Santhosh <samyukthasanthosh2203@gmail.com>
2022-02-11 19:57:29 +0530

    simple list

--- src/index.js
+++ src/index.js
@@ -2,49 +2,42 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './book.css'; 
 
-
-const firstBook={
+const books=[
+{
   img:'https://images-eu.ssl-images-amazon.com/images/I/61ck5XWxB3L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
   title:'Grandmas Bag of Stories',
   author:'Sudha Murty',
-} 
+},
 
-const secondBook={
+{
   img:'https://images-na.ssl-images-amazon.com/images/I/510vx4eIUoL._SX391_BO1,204,203,200_.jpg',
   title:'Quantitative Aptitude for Competitive Examinations',
   author:'R S Agarwal',
-} 
+} ,
 
+];
+
+const names=['john', 'peter', 'susan'];
+const newNames= names.map((name)=>{
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 
 function BookList(){
 return(
   <section className='booklist'>
-    <Book 
-    img={firstBook.img}
-    title={firstBook.title} 
-    author={firstBook.author} >
-      <p>lorem ipsum dolor sit amet consectetur adipisicing 
-      elit. Dolore cum
-      animi voluptas temporibus quo dlectus pariatur,hic
-      voluptatum
-      accusantium illo.
-      </p>
-      </Book>
-    <Book
-    img={secondBook.img}
-    title={secondBook.title} 
-    author={secondBook.author}/>
+    {newNames}
   </section>
 )
 }
 const Book=(props) =>{
-  const {img, title, author, children}=props;
+  const {img, title, author, }=props;
   return (
   <article className='book'>
     <img src={img} alt=""/>
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
+
   </article>
   )
 }
